f541ac3b2b81c3c0a4b054f9104fc3db8e82fc7c
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-10-18 13:49:41 +0100

    Update manager.ps1

--- manager.ps1
+++ manager.ps1
@@ -1,6 +1,6 @@
 ﻿$folder="teste1"
 $parent="D:\"
 $directory=$parent+$folder
-New-Item -Name $folder -Path $parent -ItemType directory
+#New-Item -Name $folder -Path $parent -ItemType directory
 $acl = Get-Acl $Directory
 $acl | fl
