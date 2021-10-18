a9d1a2b04a08fbc9bd430a2ccb85205b4a791db0
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-10-18 14:58:47 +0100

    pro

---
+++ python/demo.py
@@ -0,0 +1,9 @@
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+print(x)
+print(y)
+a = 4 # a is of type int
+a = "Sally" # a is now of type str
+print(a)
--- python/helloworld.py
+++ python/helloworld.py
@@ -1,3 +1,7 @@
 #print("hello world")
-x=input("donnez votre nom: ")
-print("hello "+x)
+""""x=input("donnez votre nom: ")
+print("hello "+x)"""
+
+if 5 > 2:
+    print("Five is greater than two!")
+    print("this is indentation")
