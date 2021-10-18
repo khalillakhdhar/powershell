9ad5847ae3fb7e760d80fb20f0a93838a8d2d296
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-10-18 10:12:26 +0100

    Initial commit

---
+++ imc.ps1
@@ -0,0 +1,16 @@
+﻿$p=Read-Host 'donner le poid'
+[float]$t=Read-Host 'donner la taille'
+[float]$imc=$p/($t*$t)
+$imc= [math]::Round($imc)
+Write-Host "votre IMC est de:" $imc
+if($imc -lt 20)
+{
+Write-Host "maigre"
+}
+elseif($imc -le 25)
+{Write-Host "idéale"
+}
+else
+{
+Write-Host "surpoids"
+}
