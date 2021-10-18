22b8e0a5e95e0cea7bd656563e4ba4b3e6896741
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-10-18 13:00:15 +0100

    Create pgcd.ps1

---
+++ pgcd.ps1
@@ -0,0 +1,18 @@
+﻿$a=Read-Host "donner le premier entier "
+Write-Host $a
+$b=Read-Host "donner le deuxiéme entier "
+Write-Host $b
+while($a -ne $b)
+{
+if($a -gt $b)
+{
+$a=$a-$b
+
+}
+else
+{
+$b=$b-$a
+}
+
+}
+Write-Host "le PGCD est : " $b
