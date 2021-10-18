063524c5e9983c7159c208c22d93047723656915
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-10-18 15:40:33 +0100

    Update ifexemple.py

---
+++ python/ifexemple.py
@@ -0,0 +1,9 @@
+a=input("donner un premier entier")
+a=int(a)
+b=int(input("donner un deuxiéme entier"))
+if a >b:
+    print("a est plus grand que b")
+elif a==b:
+    print("a et b sont égaux")
+else:
+    print("b est plus grand que a")
